d01399e6748c2d69622fbe8302d47a874027f6c3
vimu26 <akalankavimukthi2@gmail.com>
2024-02-09 22:55:00 +0530

    common redux and css added

--- web/src/scence/navbar/navbar.page.jsx
+++ web/src/scence/navbar/navbar.page.jsx
@@ -1,6 +1,30 @@
-
+// import { useState } from "react";
+// import {
+//   Box,
+//   IconButton,
+//   InputBase,
+//   Typography,
+//   Select,
+//   MenuItem,
+//   FormControl,
+//   useTheme,
+//   useMediaQuery
+// } from "@mui/material";
+// import {
+//   Search,
+//   Message,
+//   DarkMode,
+//   LightMode,
+//   Notifications,
+//   Help,
+//   Menu,
+//   Close
+// } from "@mui/icons-material";
+// import { UseDispatch, UseSelector } from "react-redux";
+// import { FlexBetween } from "components/FlexBetween.jsx";
+// import { useNavigate } from "react-router-dom";
 
 const NavbarPage = () => {
-    return (<div> This is the Navbar Page </div>);
-  };
-  export default NavbarPage
+  return <div> This is the Navbar Page </div>;
+};
+export default NavbarPage;
