b8af08e192e7495c4709fd2303254883432b7718
vimu26 <akalankavimukthi2@gmail.com>
2024-02-13 22:51:59 +0530

    nav bar completed

--- web/src/scence/navbar/navbar.page.jsx
+++ web/src/scence/navbar/navbar.page.jsx
@@ -16,7 +16,7 @@ import {
   DarkMode,
   LightMode,
   Notifications,
-  Help,
+  Help
   // Menu,
   // Close
 } from "@mui/icons-material";
@@ -35,44 +35,112 @@ const NavbarPage = () => {
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
-  // const background = theme.palette.background.default;
+  const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
   const fullName = `${user?.first_name} ${user?.last_name}`;
 
   return (
-    <Box>
-      <FlexBetween padding="1rem 6%" backgroundColor={alt}>
-        <FlexBetween gap="1.7rem">
-          <Typography
-            fontWeight="bold"
-            fontSize="clamp(1rem, 2rem, 2.5rem)"
-            color="primary"
-            onClick={() => navigate("/home")}
-            sx={{
-              "&:hover": { color: primaryLight, cursor: "pointer" }
-            }}
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.7rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.5rem)"
+          color="primary"
+          onClick={() => navigate("/home")}
+          sx={{
+            "&:hover": { color: primaryLight, cursor: "pointer" }
+          }}
+        >
+          Sociafy
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
           >
-            Sociafy
-          </Typography>
-          {isNonMobileScreens && (
-            <FlexBetween
-              backgroundColor={neutralLight}
-              borderRadius="9px"
-              gap="3rem"
-              padding="0.1rem 1.5rem"
-            >
-              <InputBase placeholder="Search" />
-              <IconButton>
-                <Search />
-              </IconButton>
-            </FlexBetween>
-          )}
-        </FlexBetween>
+            <InputBase placeholder="Search" />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
 
-        {isNonMobileScreens ? (
-          <FlexBetween gap="2rem">
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <Message sx={{ fontSize: "25px" }} />
+          <Notifications sx={{ fontSize: "25px" }} />
+          <Help sx={{ fontSize: "25px" }} />
+          <FormControl variant="standard" value={fullName}>
+            <Select
+              sx={{
+                backgroundColor: neutralLight,
+                width: "150px",
+                borderRadius: "0.25rem",
+                p: "0.25rem 1rem",
+                "& .MuiSvgIcon-root": {
+                  pr: "0.25rem",
+                  width: "3rem"
+                },
+                "& .MuiSelect-select:focus": {
+                  backgroundColor: neutralLight
+                }
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBetween>
+      ) : (
+        <IconButton
+          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+        ></IconButton>
+      )}
+
+      {/* mobile view */}
+
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position="fixed"
+          right="0"
+          bottom="0"
+          height="100%"
+          zIndex="10"
+          maxHeight="500px"
+          maxWidth="300px"
+          backgroundColor={background}
+        >
+          {/* close icon */}
+          <Box display="flex" justifyContent="flex-end" p="1rem">
+            <IconButton
+              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+            >
+              <close />
+            </IconButton>
+          </Box>
+          {/* menu items */}
+          <FlexBetween
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="3rem"
+          >
             <IconButton onClick={() => dispatch(setMode())}>
               {theme.palette.mode === "dark" ? (
                 <DarkMode sx={{ fontSize: "25px" }} />
@@ -109,13 +177,9 @@ const NavbarPage = () => {
               </Select>
             </FormControl>
           </FlexBetween>
-        ) : (
-          <IconButton
-            onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-          ></IconButton>
-        )}
-      </FlexBetween>
-    </Box>
+        </Box>
+      )}
+    </FlexBetween>
   );
 };
 
